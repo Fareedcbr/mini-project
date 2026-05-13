d036e0cc7267fe08a5aa953931db0d3c9e4b4f87
Md Fareeduddin <mdfareedcbr@gmail.com>
2026-05-13 23:25:17 +0530

    Cleanup repository: remove local env files, uploads, DB artifacts, and add .gitignore

--- frontend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'SQLALCHEMY_DATABASE_URI',
-        'mysql+pymysql://root:root@localhost/online_assignments_submission_management_system'
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
